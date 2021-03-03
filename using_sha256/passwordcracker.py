38d8304820243849586dc0a6f1a97758535a4039
Austinstevesk <austinstevesk@gmail.com>
2021-03-04 00:21:20 +0300

    Added a sha256 file

--- using_sha256/passwordcracker.py
+++ using_sha256/passwordcracker.py
@@ -6,9 +6,13 @@ with open('comm_passwords.txt', 'r') as f:
 with open('username_hashpass.txt', 'r') as t:
     text = t.read().splitlines()
     for userhash in text:
+        print(userhash)
         username = userhash.split(":")[0]
         print(username)
         hashcode = userhash.split(":")[1]
         user_hash_dict[username] = hashcode
+
+for user, hash in user_hash_dict.items():
+    print(user, hash)
 
 
